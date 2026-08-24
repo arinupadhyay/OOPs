4963bcd564179988ac561db446aec85b3e89dc3e
arinupadhyay <arinupadhyay524@gmail.com>
2026-08-24 23:45:07 +0530

    Fix typo in voterID variable name

--- basic.cpp
+++ basic.cpp
@@ -14,7 +14,7 @@ int main() {
     v1.voterID = 101;
     v1.name = "Rahul";
 
-    cout << "Voter ID: " << v1.voterID << endl;
+    cout << "Voter ID: " << v1.voterid << endl;
     cout << "Name: " << v1.name << endl;
 
     return 0;
