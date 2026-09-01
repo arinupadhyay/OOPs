d01bdafb15c2dd7d85eef8ff49639d22e5e0f07e
arinupadhyay <arinupadhyay524@gmail.com>
2026-09-01 23:32:44 +0530

    Update abstraction.cpp

--- abstraction.cpp
+++ abstraction.cpp
@@ -5,12 +5,12 @@ class Car {
 public:
     void start() {
         engine();
-        cout << "Car started" << endl;
+        cout << "car started:  \t" << endl;
     }
 
 private:
     void engine() {
-        cout << "Engine started" << endl;
+        cout << "engine started:   \t" << endl;
     }
 };
 
